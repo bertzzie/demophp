9224ae9d984321e0a012a7acf1eea4f280c4d1ff
Alex Xandra Albert Sim <bertzzie@gmail.com>
2013-12-13 12:18:57 +0700

    Commit pertama.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$mysqli = new mysqli("localhost", "userframework", "password", "framework");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+echo $mysqli->host_info . "\n";
+
+$res = $mysqli->query("SELECT email FROM pengguna ORDER BY id ASC");
+
+echo "Reverse order...\n";
+for ($row_no = $res->num_rows - 1; $row_no >= 0; $row_no--) {
+    $res->data_seek($row_no);
+    $row = $res->fetch_assoc();
+    echo " id = " . $row['email'] . "\n";
+}
+
